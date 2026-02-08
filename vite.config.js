c595a1e12ff139fda9b4658accf363d8205610e4
Ian Victor Joseph Murray <42899620+imurra@users.noreply.github.com>
2026-02-07 18:58:55 -0600

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",   // ← THIS is what makes GitHub Pages work
+  plugins: [react()],
+  base: "/pv-loop-simulator/", // or "./" if you prefer
 });
